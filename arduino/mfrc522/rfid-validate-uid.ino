1d9ab6d565f88801a90bde6c8cc275d4205307db
Suresh Balasubramaniam <sukucomp@gmail.com>
2026-04-02 01:34:25 +0800

    Add RFID UID validation with OLED display
    This code initializes an RFID reader and an OLED display, reads UID from RFID cards, and indicates whether the UID is valid or invalid using a buzzer and display messages.

---
+++ arduino/mfrc522/rfid-validate-uid.ino
@@ -0,0 +1,119 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//Display Settings
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define RST_PIN         9          // Configurable, see typical pin layout above
+#define SS_PIN          10         // Configurable, see typical pin layout above
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+const int buzzer = 7; //buzzer to arduino pin 7
+String uidString;
+
+void setup() {
+	Serial.begin(9600);		// Initialize serial communications with the PC
+	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+	SPI.begin();			// Init SPI bus
+	mfrc522.PCD_Init();		// Init MFRC522
+ 
+	Serial.println(F("Mifare Classic Card Details"));
+
+  //LCD Initialization 
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.display();
+  display.setTextColor(WHITE); // or BLACK);
+  display.setTextSize(2);
+  display.setCursor(10,0); 
+  display.print("RFID LOCK");
+  display.display();
+  
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop() {
+	// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+	if ( ! mfrc522.PICC_IsNewCardPresent()) {
+		return;
+	}
+
+	// Select one of the cards
+	if ( ! mfrc522.PICC_ReadCardSerial()) {
+   //;
+		return;
+	}
+
+	// Dump debug info about the card; PICC_HaltA() is automatically called
+   mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+
+//Converting UID to Hex
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  //Displaying UID to LCD
+  content.toUpperCase();
+  Serial.print(content.substring(1));
+  if(content.substring(1)!=uidString){
+     clearUID();
+     uidString=content.substring(1); 
+     if(uidString=="21 55 2B 1A" || uidString=="E2 DE 05 19"){
+      ValidUID();
+     }else{
+      inValidUID(); 
+     }
+  }
+ printUID();
+ 
+}//End LOOP
+
+void ValidUID(){
+  display.clearDisplay();
+  display.display();
+  display.setTextColor(WHITE); // or BLACK);
+  display.setTextSize(2);
+  display.setCursor(10,0); 
+  display.print("Valid UID");
+  display.display();
+  //BUZZER  
+  tone(buzzer, 100);delay(100);noTone(buzzer);
+}
+void inValidUID(){
+  display.clearDisplay();
+  display.display();
+  display.setTextColor(WHITE); // or BLACK);
+  display.setTextSize(2);
+  display.setCursor(5,0); 
+  display.print("InValidUID");
+  display.display();
+  //BUZZER  
+  tone(buzzer, 300);delay(1000);noTone(buzzer);
+}
+  void clearUID()
+  {
+    display.setTextColor(BLACK); // or BLACK);
+    display.setTextSize(1);
+    display.setCursor(30,20); 
+    display.print(uidString);
+    display.display();
+  }
+
+  void printUID()
+  {
+    display.setTextColor(WHITE); // or BLACK);
+    display.setTextSize(1);
+    display.setCursor(0,20); 
+    display.print("UID: ");
+    display.setCursor(30,20); 
+    display.print(uidString);
+    display.display();
+  }
